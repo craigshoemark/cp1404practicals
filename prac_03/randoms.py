1cd84d5dd9fae9d4c268b8385110f9ee520d94e0
craigshoemark <craig.shoemark@my.jcu.edu.au>
2024-06-06 22:13:59 +1000

    Complete console and random module exercises

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# line 1
+# Smallest = 5
+# Largest = 20
+
+# line 2
+# Smallest = 3
+# Largest = 9
+# No
+
+# line 3
+# 2.5000000000000000
+# 5.4999999999999999
+
+import random
+
+print(random.randint(1, 100))
