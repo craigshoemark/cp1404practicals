a67c9c2ecc480ae1091b27037948ed86bfd1afd2
craigshoemark <craig.shoemark@my.jcu.edu.au>
2024-07-17 10:27:18 +1000

    Add menu to project_management.py
    - Add FILENAME constant for projects.txt
    - Add MENU constant
    - Add menu skeleton code
    - Initialise projects.txt in main()
    - Add welcome message

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -11,17 +11,47 @@ Actual time:
 # Datetime
 
 # CONSTANTS:
-# MENU
+# FILENAME = projects.txt
+MENU = """
+(L)oad projects\n(S)ave projects\n(D)isplay projects \n(F)ilter projects by date 
+(A)dd new project \n(U)pdate project \n(Q)uit
+"""
 
 
 def main():
+    projects = []
+
     # TODO Menu
+    print("Welcome to Pythonic Project Management")
+    # Display loaded objects summary
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != 'Q':
+        if choice == 'L':
+            pass
+        elif choice == 'S':
+            pass
+        elif choice == 'D':
+            pass
+        elif choice == 'F':
+            pass
+        elif choice == 'A':
+            pass
+        elif choice == 'U':
+            pass
+        elif choice == 'Q':
+            pass
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
     pass
 
 
 def load_projects():
     # TODO Prompt the user for a filename to load projects from and load them
     # Load from file projects.txt
+    # Projects will be objects
     pass
 
 
