447140d3d98a1f9bc70fffb8c1f41e4122ecec56
craigshoemark <craig.shoemark@my.jcu.edu.au>
2024-07-17 10:03:46 +1000

    Project outline: Define class functions and client code functions
    - Define class functions in project.py
    - Define client code functions in project_management.py

---
+++ prac_07/project.py
@@ -0,0 +1,19 @@
+"""Project Class."""
+
+
+class Project:
+    def __init__(self):
+        # TODO add instance variables
+        pass
+
+    def __str__(self):
+        # TODO
+        pass
+
+    def __repr__(self):
+        # TODO
+        pass
+
+    def sort_projects(self):
+        # TODO sort projects based on priority order
+        pass
---
+++ prac_07/project_management.py
@@ -0,0 +1,68 @@
+"""
+Project management program
+
+Estimate time for completion: 10 hours.
+Actual time:
+"""
+
+
+# Imports:
+# Class
+# Datetime
+
+# CONSTANTS:
+# MENU
+
+
+def main():
+    # TODO Menu
+    pass
+
+
+def load_projects():
+    # TODO Prompt the user for a filename to load projects from and load them
+    # Load from file projects.txt
+    pass
+
+
+def display_projects():
+    # TODO Display two groups: incomplete projects; completed projects, both sorted by priority
+
+    # TODO Incomplete projects: own function ?
+    # Display incomplete projects, name, date, priority(sort by), estimate, completion percentage.
+    # See sample output.
+    # TODO Completed projects: own function ?
+    # Display incomplete projects, name, date, priority, estimate, completion percentage.
+    # See sample output.
+    pass
+
+
+def update_project():
+    # Choose a project, then modify the completion % and/or priority - leave blank to retain existing values
+    # TODO enumerate (start 0), name, start date, priority, estimate, completion %,
+    #  add new completion %, add new priority (by number).
+    # See sample output.
+    pass
+
+
+def add_new_project():
+    # Ask the user for the inputs and add a new project to memory
+    # TODO add project name, start date, priority, cost estimate, percent completed
+    # See sample output.
+    pass
+
+
+def filter_projects_by_date():
+    # TODO show projects after (x date),
+    # TODO display name, start date, priority, estimate cost, completion percentage %
+    # See sample output.
+    pass
+
+
+def save_projects():
+    # TODO Prompt the user for a filename to save projects to and save them
+    #
+    pass
+
+
+main()
