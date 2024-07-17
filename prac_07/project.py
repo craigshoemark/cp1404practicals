2d2b1ecbb708a50233f42107848e5c03799d98de
craigshoemark <craig.shoemark@my.jcu.edu.au>
2024-07-17 10:58:19 +1000

    Add instance variables to classProject
    - Add __init__ docstring
    - Add __str__ return statement and docstring
    - Add __repr__ return statement and docstring

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,17 +2,25 @@
 
 
 class Project:
-    def __init__(self):
-        # TODO add instance variables
-        pass
+    def __init__(self, name, start_date, priority, cost_estimate, completion):
+        """classProject instance variables."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion = completion
 
     def __str__(self):
-        # TODO
-        pass
+        """Display string values."""
+        # TODO - will be int and float values here..
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate{self.cost_estimate:.2f}, completion: {self.completion}")
 
     def __repr__(self):
-        # TODO
-        pass
+        """Display representation."""
+        # TODO - will be int and float values here..
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
+                f"estimate{self.cost_estimate:.2f}, completion: {self.completion}")
 
     def sort_projects(self):
         # TODO sort projects based on priority order
