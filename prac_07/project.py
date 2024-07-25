61a3b5704963bce927ab07e158c60610551ed688
craigshoemark <craig.shoemark@my.jcu.edu.au>
2024-07-25 13:33:06 +1000

    Remove TODO from project.py
    - __str__
    - __repr__

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,6 @@
 """Project Class."""
 
 
-# def sort_by_priority(projects):
-#     projects.sort()
-
-
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion):
         """classProject instance variables."""
@@ -16,13 +12,11 @@ class Project:
 
     def __str__(self):
         """Display string values."""
-        # TODO - will be int and float values here..
         return (f"{self.name}, start: {self.start_date}, priority: {self.priority}, "
                 f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion}%")
 
     def __repr__(self):
         """Display representation."""
-        # TODO - will be int and float values here..
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, "
                 f"estimate{self.cost_estimate:.2f}, completion: {self.completion}")
 
