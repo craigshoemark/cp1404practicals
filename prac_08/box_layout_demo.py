e3e5d549c255eb8df7b3e9d6c52e5d97e92ca219
craigshoemark <craig.shoemark@my.jcu.edu.au>
2024-07-27 13:57:27 +1000

    Complete modify existing GUI program exercise
    - box_layout.py
    - box_layout_demo.py

---
+++ prac_08/box_layout_demo.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        """Handle text state on button press."""
+        print("Greet")
+        self.root.ids.greet_button.text = "Hello :)"
+
+        print("Test")
+        self.root.ids.output_label.text = "Hello"
+
+        print("Label test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def clear_fields(self):
+        self.root.ids.input_name.text = " "
+        self.root.ids.greet_button.text = "Greet"
+        self.root.ids.output_label.text = "Enter your name"
+
+
+BoxLayoutDemo().run()
